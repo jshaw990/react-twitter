013ba8e83eba93f67b729dc918a9617dd9f37ae5
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-14 11:41:42 -0600

    Tweet component partial build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { InMemoryCache } from 'apollo-boost'
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient ({
+  link: 'http://localhost:5000/graphiql',
+  cache
+})
+
+const AppContainer = () => (
+  <ApolloProvider client={client}>
+  <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<AppContainer />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
